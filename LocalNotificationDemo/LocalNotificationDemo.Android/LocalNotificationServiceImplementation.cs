703e9b765c0bee6cfe499a6b73da6fc4ecf85b6c
Elvis Xia (Shang Hai Wei Chuang Ruan Jian) <v-xucxia@microsoft.com>
2017-06-21 08:18:59 +0800

    Add project files.

---
+++ LocalNotificationDemo/LocalNotificationDemo.Android/LocalNotificationServiceImplementation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using LocalNotificationDemo.Droid;
+
+[assembly:Dependency(typeof(LocalNotificationServiceImplementation))]
+namespace LocalNotificationDemo.Droid
+{
+    public class LocalNotificationServiceImplementation :Java.Lang.Object, ILocalNotificationService
+    {
+        public void ShowLocalNotification(string title, string text, string icon)
+        {
+            
+            // Instantiate the builder and set notification elements:
+            Notification.Builder builder = new Notification.Builder(Forms.Context)
+                .SetContentTitle(title)
+                .SetContentText(text);
+
+            if (icon == "ic_notification")
+            {
+                builder.SetSmallIcon(Resource.Drawable.ic_notification);
+            }
+
+            // Build the notification:
+            Notification notification = builder.Build();
+
+            // Get the notification manager:
+            NotificationManager notificationManager =
+                Forms.Context.GetSystemService(Context.NotificationService) as NotificationManager;
+
+            // Publish the notification:
+            const int notificationId = 0;
+            notificationManager.Notify(notificationId, notification);
+        }
+    }
+}
---
+++ LocalNotificationDemo/LocalNotificationDemo/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LocalNotificationDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DependencyService.Get<ILocalNotificationService>().ShowLocalNotification("My Title", "Hello World! This is my first notification!", "ic_notification");
+        }
+    }
+}
